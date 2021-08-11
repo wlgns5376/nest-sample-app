25dc80d4e84290fce21286d831f6d7b7a4af5d8b
Ji hoon Kang <wlgns5376@naver.com>
2021-08-11 18:07:22 +0900

    Set static assets and port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+  const staticPath = configService.get<string>('STATIC_PATH');
+  
+  app.useStaticAssets(join(__dirname, '..', staticPath), {
+    prefix: `/${staticPath}/`,
+  });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
